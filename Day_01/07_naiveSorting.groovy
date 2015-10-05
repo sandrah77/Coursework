c3ecf258b9adbe76adf814deb882e3d722c5227b
Stephen Farmer <safarmer@icloud.com>
2015-10-05 12:23:30 +0100

    Using selection sort algorithm

--- Day_01/07_naiveSorting.groovy
+++ Day_01/07_naiveSorting.groovy
@@ -3,4 +3,29 @@ Write a program that reads three numbers and prints them in order,
 from lowest to highest.
 */
 
-String str1 = System.console().readLine();
+int temp;
+int[] numbers = new int [3];
+println "Please enter " + numbers.length + " numbers to be sorted.";
+for (int x = 0; x < numbers.length - 1; x++) {
+	// find the lowest number in the list, using Selection Sort.
+
+	println	"Enter a number: "
+	String str = System.console().readLine();
+	numbers[x] = Integer.parseInt(str);
+		
+		if (numbers[2] < numbers[1]) {
+		temp = numbers[2];
+		numbers[2] = numbers[1];
+		numbers[1] = temp;
+		}
+		if (numbers[1] < number[0]) {
+			temp = numbers[1];
+			numbers[1] = numbers[0];
+			numbers[0] = temp;
+		}
+		if (numbers[0] > number[2]) {
+			temp = numbers[0];
+			numbers[0] = numbers[2];
+			numbers[]
+		}
+}		
