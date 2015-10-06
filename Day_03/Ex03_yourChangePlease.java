8ea133e386ab57e934ad6db2f7f6f7adb82710ab
Stephen Farmer <safarmer@icloud.com>
2015-10-06 22:46:03 +0100

    Revision to file names and calculators

---
+++ Day_03/Ex03_yourChangePlease.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ex03_yourChangePlease {
+	public static void main (String[] args) {
+		
+		// Read text from the user
+		Scanner input = new Scanner(System.in);
+		System.out.println ("Please enter the total cost of scanned items for confirmation: ");
+		double totalCost = input.nextDouble();
+		
+		int remainingAmount = (int) totalCost * 100;
+
+		// Find the number of one dollars
+		int oneDollarBills = remainingAmount / 100;
+		remainingAmount = remainingAmount % 100;
+
+		System.out.println ("Please enter the amount being payed..");
+		if
+
+		// Perform math operation on two numbers
+	}
+
+}
+
