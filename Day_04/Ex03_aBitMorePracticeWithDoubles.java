34a3d9917cac11c3df3471adf5697479dc5e9f2b
Stephen Farmer <safarmer@icloud.com>
2015-10-11 12:36:48 +0100

    Working on methods

---
+++ Day_04/Ex03_aBitMorePracticeWithDoubles.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Ex03_aBitMorePracticeWithDoubles {
+
+	public static void main (String [] args) {
+
+	Scanner input = new Scanner(System.in);
+
+	System.out.println("Please enter the total amount borrowed ")
+
+
+
+	}
+
+public static class mortgageCalculator {
+
+	double totalAmountDue (String amount) {
+
+
+	}
+
+	double annualAmountDue (String yearly) {
+
+
+
+	}
+
+	double interestFree (String interest) {
+
+
+	}
+
+
+} 
+
+
+}
