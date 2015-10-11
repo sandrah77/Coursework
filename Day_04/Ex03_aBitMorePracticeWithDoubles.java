1fb345bebe7d6b598e3a2809b5075e1a3384ccdd
Stephen Farmer <safarmer@icloud.com>
2015-10-11 14:51:24 +0100

    working on interest is paid method

--- Day_04/Ex03_aBitMorePracticeWithDoubles.java
+++ Day_04/Ex03_aBitMorePracticeWithDoubles.java
@@ -4,34 +4,68 @@ public class Ex03_aBitMorePracticeWithDoubles {
 
 	public static void main (String [] args) {
 
-	Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
+		MortgageCalculator calculator = new MortgageCalculator();
 
-	System.out.println("Please enter the total amount borrowed ")
+		System.out.println("Please enter the total amount borrowed on your mortgage " +
+			" in decimal notation (ie: 95000.50) : ");
 
+		double totalBorrowed = input.nextDouble();
 
+		System.out.println("What is the number of year(s) given to pay the loan: " + 
+			" (ie: 5)");
 
+		int numberOfYears = input.nextInt();
+
+		System.out.println("Please enter the interest rate for the loan as a decimal " +
+			" percent (ie: 5.42)");
+
+		double interestRate = input.nextDouble();
+
+		calculator.totalAmountDue(totalBorrowed, interestRate);
+		calculator.annualAmountDue(numberOfYears);
+
+		double myTotal = calculator.totalReturn();
+		double myAnnualAmount = calculator.annualAmountReturn();
+		int myInterestIsPaid = calculator.interestPaid();
+
+		System.out.println ("Your total amount to be paid is: " + myTotal);
+		System.out.println ("The amount of money to be paid yearly is: " + myAnnualAmount);
+		System.out.println ("The amount of years before all the interest is paid and only" +
+		 	" the initial capital remains is: " + myInterestIsPaid);
 	}
 
-public static class mortgageCalculator {
+	public static class MortgageCalculator {
+		private double total;
+		private double annualAmount;
+		private int interestFreeYears;
 
-	double totalAmountDue (String amount) {
+			public void totalAmountDue (double amountBorrowed, double interest) {
+				amountBorrowed = amountBorrowed * (1 + (interest / 100);  
+				total = amountBorrowed;
+			}
+
+			public void annualAmountDue (int accumulator) {
+				annualAmount += (double) total / accumulator;
+			}
+
+			public void interestFree (String interest) {
 
 
+				interestFreeYears = total 
+
+			}
+
+			public double totalReturn() {
+				return total;
+			}
+			
+			public double annualAmountReturn() {
+				return annualAmount;
+			}
+			
+			public double interestPaid() {
+				return interestFreeYears;
+			}
 	}
-
-	double annualAmountDue (String yearly) {
-
-
-
-	}
-
-	double interestFree (String interest) {
-
-
-	}
-
-
 } 
-
-
-}
