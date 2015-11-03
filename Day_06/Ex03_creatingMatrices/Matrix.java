39d998726d658be492ba1d3c8682633fc3e2ce95
Stephen Farmer <safarmer@icloud.com>
2015-11-03 23:07:10 +0000

    running tests..

--- Day_06/Ex03_creatingMatrices/Matrix.java
+++ Day_06/Ex03_creatingMatrices/Matrix.java
@@ -33,44 +33,84 @@ modifies its elements (one-by-one, by rows, and by columns), and prints the matr
 */
 
 public class Matrix {
-	public static void main (String[] args) {
-		int[][] myMatrix; // declare a matrix of type int
+	private int[][] myMatrix; // declare a matrix of type int
 
-		test(5,10);
-		System.out.println (myMatrix[row][col] + " ");
-	}
+	// Construct the 2-D Matrix to hold the data with given size. 
+	public Matrix(int arrSize1, int arrSize2) { 
+		this.myMatrix = new int[arrSize1][arrSize2];
 
-	public void test(int row, int col) { 
-		myMatrix = new int[row][col];
-		for (int row = 0; row < myMatrix[].length; row++) {
-			for (int col = 0; col < myMatrix[].length; col++) {
-				myMatrix[row][col] = 1;
-				
+		for (int i = 0; i < arrSize1; i++) {
+			for (int j = 0; j < arrSize2; j++) {
+				this.myMatrix[i][j] = 1;
 			}
-		}	
+		}
 	}
-}
 
+	public void setElement(int row, int col, int newValue) {
+		int rowLength = this.myMatrix.length;
+		int colLength = this.myMatrix.length;
+		if ((row <= rowLength) && (col <= colLength)) {
+				this.myMatrix[row][col] = newValue;
+		}
+	}	
 
-/*
-setElement(int row int col int newValue){
-
-}
-
-setRow(int row , String) {
-
-}
-
-setColumn(int col, String) { 
-
-}
-
-toString() {
-
-
-}
-
-prettyPrint() {
-*/
-
+	public void setRow(int row, String userNums) {
+		int rowLength = this.myMatrix.length;
+		if (row <= rowLength) { // check that the indices are within a valid length
+			userNums = userNums.replaceAll("\\D+",""); // use the shorthand character class to match [0-9]
+			if (userNums.length() == rowLength) {
+				for (int i = 0; i < rowLength; i++) {
+					this.myMatrix[row][i] = Character.getNumericValue(userNums.charAt(i));
+				}
+			}
+		}
+	}
 	
+	public void setColumn(int col, String userNums) {
+		int colLength = this.myMatrix.length;
+		if (col <= colLength) { // check that the indices are within a valid length
+			userNums = userNums.replaceAll("\\D+",""); // use the shorthand character class to match [0-9]
+			if (userNums.length() == colLength) {
+				for (int i = 0; i < colLength; i++) {
+					this.myMatrix[i][col] = Character.getNumericValue(userNums.charAt(i));
+				}
+			}
+		}
+	}	
+	
+	public String toString() {
+		String values = "";
+		 values+= "[";
+		for(int i = 0; i < this.myMatrix.length; i++) {
+			for (int j = 0; j < this.myMatrix[i].length; j++) {
+				values = values.substring(0, values.length()-1); 
+				values += "]";
+			}
+		}
+		return values; 
+	}
+	
+	/**
+	* Print formatted matrix to console
+	*/
+	public void prettyPrint() {
+		for (int i = 0; i < this.myMatrix.length; i++) { 
+			for (int j = 0; j < this.myMatrix[j].length; j++) {
+				System.out.print(this.myMatrix[i][j] + "\t");
+			}
+			System.out.println();
+		}	 
+	}
+	
+	public static void main (String[] args) {
+
+	Matrix myMatrix = new Matrix(5, 5);
+	System.out.println(myMatrix.toString());	
+	//myMatrix.setElement(5,9,20);
+	//myMatrix.setRow(0, "1,2,3");
+	//myMatrix.setColumn(1, "4,5,6,7");
+	//myMatrix.prettyPrint();
+
+
+	}	
+}
